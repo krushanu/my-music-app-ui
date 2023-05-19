d06b3f9734571cada212a972904349df4e25bf75
Krushanu Mohapatra <mohapatrakrushanu@gmail.com>
2023-05-19 11:11:53 -0400

    Home Page navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import logo from './../logo.svg';
 import './../App.css';
-import { Button, Grid } from '@mui/material';
-import { Link } from 'react-router-dom';
+import ButtonBases from '../components/ButtonBases';
 
 const Home = () => {
 
@@ -10,17 +9,7 @@ const Home = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <Grid container >
-          <Grid >
-            <Button variant="contained" size="large"><Link to="/artist">Artist</Link></Button>
-          </Grid>
-          <Grid>
-            <Button variant="contained" size="large"><Link to="/album">Album</Link></Button>
-          </Grid>
-          <Grid>
-            <Button variant="contained" size="large"><Link to="/song">Song</Link></Button>
-          </Grid>
-        </Grid>
+        <ButtonBases />
       </header>
     </div>
   );
